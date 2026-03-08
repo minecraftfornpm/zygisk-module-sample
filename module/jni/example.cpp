eadde754072af072bad3acab496d5b746c95e486
minecraftfornpm <abobatopaha@gmail.com>
2026-03-08 09:34:52 +0400

    Update example.cpp

--- module/jni/example.cpp
+++ module/jni/example.cpp
@@ -1,79 +1,51 @@
-/* Copyright 2022-2023 John "topjohnwu" Wu
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
-
-#include <cstdlib>
-#include <unistd.h>
-#include <fcntl.h>
 #include <android/log.h>
-
+#include <dlfcn.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include "zygisk.hpp"
 
-using zygisk::Api;
-using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "BPM_Injector", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "BPM_Injector", __VA_ARGS__)
 
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
+#define TARGET_PACKAGE "com.skullcapstudios.bpm"
+#define LIBRARY_PATH "/data/local/tmp/libmain.so"
 
 class MyModule : public zygisk::ModuleBase {
 public:
-    void onLoad(Api *api, JNIEnv *env) override {
+    void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
-        this->env = env;
     }
 
-    void preAppSpecialize(AppSpecializeArgs *args) override {
-        // Use JNI to fetch our process name
-        const char *process = env->GetStringUTFChars(args->nice_name, nullptr);
-        preSpecialize(process);
-        env->ReleaseStringUTFChars(args->nice_name, process);
+    void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
+        const char *process = args->nice_name ? args->nice_name : args->app_data_dir;
+        
+        if (process && strstr(process, TARGET_PACKAGE)) {
+            LOGD("Найдена целевая игра: %s", process);
+            
+            if (access(LIBRARY_PATH, F_OK) == -1) {
+                LOGE("Библиотека не найдена по пути: %s", LIBRARY_PATH);
+                return;
+            }
+            
+            void *handle = dlopen(LIBRARY_PATH, RTLD_NOW);
+            if (handle) {
+                LOGD("Библиотека успешно загружена!");
+                void (*init_func)() = (void (*)()) dlsym(handle, "init");
+                if (init_func) {
+                    LOGD("Вызываем функцию init()");
+                    init_func();
+                }
+            } else {
+                LOGE("Ошибка загрузки библиотеки: %s", dlerror());
+            }
+        }
     }
 
-    void preServerSpecialize(ServerSpecializeArgs *args) override {
-        preSpecialize("system_server");
-    }
+    void postAppSpecialize(zygisk::AppSpecializeArgs *args) override {}
 
 private:
-    Api *api;
-    JNIEnv *env;
-
-    void preSpecialize(const char *process) {
-        // Demonstrate connecting to to companion process
-        // We ask the companion for a random number
-        unsigned r = 0;
-        int fd = api->connectCompanion();
-        read(fd, &r, sizeof(r));
-        close(fd);
-        LOGD("process=[%s], r=[%u]\n", process, r);
-
-        // Since we do not hook any functions, we should let Zygisk dlclose ourselves
-        api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-    }
-
+    zygisk::Api *api = nullptr;
 };
 
-static int urandom = -1;
-
-static void companion_handler(int i) {
-    if (urandom < 0) {
-        urandom = open("/dev/urandom", O_RDONLY);
-    }
-    unsigned r;
-    read(urandom, &r, sizeof(r));
-    LOGD("companion r=[%u]\n", r);
-    write(i, &r, sizeof(r));
-}
-
-// Register our module class and the companion handler function
 REGISTER_ZYGISK_MODULE(MyModule)
-REGISTER_ZYGISK_COMPANION(companion_handler)
