67e01ebde1fe10efa8942961f5f83e3321227105
minecraftfornpm <abobatopaha@gmail.com>
2026-03-08 09:48:06 +0400

    Update Application.mk

--- module/jni/Application.mk
+++ module/jni/Application.mk
@@ -1,4 +1,3 @@
-APP_ABI      := armeabi-v7a arm64-v8a x86 x86_64
-APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden
-APP_STL      := none
-APP_PLATFORM := android-21
+APP_ABI := arm64-v8a
+APP_PLATFORM := android-23
+APP_STL := none
