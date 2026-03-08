1d2eaf8fb835b07b7aa48ee591ee6d902b2dc541
minecraftfornpm <abobatopaha@gmail.com>
2026-03-08 09:53:56 +0400

    Update example.cpp

--- module/jni/example.cpp
+++ module/jni/example.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <jni.h>
 #include "zygisk.hpp"
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "BPM_Injector", __VA_ARGS__)
@@ -15,19 +16,25 @@ class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
+        this->env = env;
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-        const char *process = args->nice_name ? args->nice_name : args->app_data_dir;
-        
+        const char *process = nullptr;
+        if (args->nice_name != nullptr) {
+            process = env->GetStringUTFChars(args->nice_name, nullptr);
+        } else if (args->app_data_dir != nullptr) {
+            process = env->GetStringUTFChars(args->app_data_dir, nullptr);
+        }
+
         if (process && strstr(process, TARGET_PACKAGE)) {
             LOGD("Найдена целевая игра: %s", process);
-            
+
             if (access(LIBRARY_PATH, F_OK) == -1) {
                 LOGE("Библиотека не найдена по пути: %s", LIBRARY_PATH);
-                return;
+                goto cleanup;
             }
-            
+
             void *handle = dlopen(LIBRARY_PATH, RTLD_NOW);
             if (handle) {
                 LOGD("Библиотека успешно загружена!");
@@ -40,12 +47,20 @@ public:
                 LOGE("Ошибка загрузки библиотеки: %s", dlerror());
             }
         }
+
+    cleanup:
+        if (args->nice_name != nullptr && process != nullptr) {
+            env->ReleaseStringUTFChars(args->nice_name, process);
+        } else if (args->app_data_dir != nullptr && process != nullptr) {
+            env->ReleaseStringUTFChars(args->app_data_dir, process);
+        }
     }
 
-    void postAppSpecialize(zygisk::AppSpecializeArgs *args) override {}
+    void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {}
 
 private:
     zygisk::Api *api = nullptr;
+    JNIEnv *env = nullptr;
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
